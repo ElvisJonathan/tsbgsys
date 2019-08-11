8b1561cc1393e3a957c6f3f97e434e880083c3b2
ElvisJonathan <chenq0711@sina.com>
2019-08-11 14:26:38 +0800

    修改权限相关修改

--- src/main/java/com/tsbg/ecosys/controller/JurisdictionController.java
+++ src/main/java/com/tsbg/ecosys/controller/JurisdictionController.java
@@ -1,7 +1,7 @@
 package com.tsbg.ecosys.controller;
 
 import com.tsbg.ecosys.config.ResultResponse;
-import com.tsbg.ecosys.model.EperRole;
+import com.tsbg.ecosys.model.PowerPackage;
 import com.tsbg.ecosys.model.EuserInfo;
 import com.tsbg.ecosys.service.EperRoleService;
 import com.tsbg.ecosys.service.EuserInfoService;
@@ -9,8 +9,7 @@ import com.tsbg.ecosys.service.EuserRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 权限相关
@@ -49,12 +48,6 @@ public class JurisdictionController {
                     List<Integer> pid = eperRoleService.selectPidByRid(rid);
                     //如果pid不为null,则把查询出的list返回给前端
                     if (pid != null){
-                        //将list数据存到数组返回
-                        /*int[] Array = new int[5];
-                        for (int i=0;i<=pid.size()-1;i++){
-                            //循环遍历赋值
-                            Array[i]=pid.get(i);
-                        }*/
                         //将用户名和pid返给前端
                         resultResponse = new ResultResponse(0,"提示信息：成功查询到权限信息",pid,uName);
                         return resultResponse;
@@ -81,36 +74,58 @@ public class JurisdictionController {
      */
     @RequestMapping(value = "/updatepower", method = { RequestMethod.GET, RequestMethod.POST })
     @ResponseBody
-    public ResultResponse updatePower(@RequestBody List pod){
+    public ResultResponse updatePower(@RequestBody PowerPackage powerPackage){
         //初始化构造器
         ResultResponse resultResponse = null;
-        //pid中包含工号和修改后的四个权限ID
-        //模拟一波传过来的List 假设list中有[1,6,6,6,"F1336602"]五个值
-        //此模拟修改以最终与前端调试结果为准
-        List<Object> pid = new ArrayList<Object>();
-        pid.add(1);pid.add(6);pid.add(6);pid.add(6);pid.add("F1336602");
-        //根据传过来的userCode查询对应的uid
-        String userCode = pid.get(pid.size()-1).toString();
-        int uid = euserInfoService.selectuidbyuserCode(userCode);
-        //通过uid查询用户对应的角色rid
-        int rid = euserRoleService.selectRidByUid(uid);
-        //根据rid去查询对应的prid,通过prid去修改pid
-        List prid = eperRoleService.selectPridByRid(rid);
-        //for循环修改权限
-        for (int i=0;i<prid.size();i++){
-            //调用修改方式循环遍历修改所有权限ID
-            int num = eperRoleService.updatePowerByPrid(pid.get(i),prid.get(i));
-            if (num>0){
-                //遍历次数决定输出的成功次数，有一次失败则修改不完全
-                System.out.println("修改成功一次");
-            }else{
-                //如果某个权限id修改失败返回失败信息并中止修改过程
-                resultResponse = new ResultResponse(501,"提示信息：修改失败,请检查权限设置！");
-                return resultResponse;
+        //需要获取工号和修改后的四个权限ID
+        //成功获取了工号
+        String userCode = powerPackage.getEuserInfo().getUserCode();
+        //成功获取了data数组
+        String[] data = powerPackage.getData();
+        //创建alist用于存data值
+        List<String> alist = new ArrayList<String>();
+        //循环遍历加元素
+        for (int i=0;i<=data.length-1;i++){
+            //不为空的情况下加
+            if (data[i]!=null){
+                alist.add(data[i].substring(6,7));
             }
         }
-        //最终返回修改成功的提示给前端
-        resultResponse = new ResultResponse(0,"提示信息：修改成功！");
+        //验证输出的alist
+        System.out.println("输出alist值："+alist.toString());
+        //根据传过来的userCode查询对应的uid
+        Integer uid = euserInfoService.selectuidbyuserCode(userCode);
+        if (uid!=null){
+            //通过uid查询用户对应的角色rid
+            Integer rid = euserRoleService.selectRidByUid(uid);
+            if (rid!=null){
+                //根据rid去查询对应的prid,通过prid去修改pid
+                List prid = eperRoleService.selectPridByRid(rid);
+                if (prid!=null){
+                    //for循环修改权限
+                    for (int i=0;i<prid.size();i++){
+                        //调用修改逻辑循环遍历修改所有权限ID
+                        int num = eperRoleService.updatePowerByPrid(alist.get(i),prid.get(i));
+                        if (num==0){
+                            //如果某个权限id修改失败返回失败信息并中止修改过程
+                            resultResponse = new ResultResponse(501,"提示信息：修改失败,请检查权限设置！");
+                            return resultResponse;
+                        }
+                    }
+                    //最终返回修改成功的提示给前端
+                    resultResponse = new ResultResponse(0,"提示信息：修改成功！");
+                    return resultResponse;
+                }
+                //prid为空返回异常信息
+                resultResponse = new ResultResponse(502,"提示信息：修改异常,未查到对应权限！");
+                return resultResponse;
+            }
+            //rid为空返回异常信息
+            resultResponse = new ResultResponse(503,"提示信息：未查询到用户角色信息！");
+            return resultResponse;
+        }
+        //uid为空返回异常信息
+        resultResponse = new ResultResponse(504,"提示信息：未查询到用户编号信息！");
         return resultResponse;
     }
 }
