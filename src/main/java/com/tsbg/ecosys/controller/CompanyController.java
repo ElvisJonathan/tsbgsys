dd69d86ea8e4e2e51939619b35b57472f4001b18
ElvisJonathan <chenq0711@sina.com>
2019-08-12 15:50:59 +0800

    新增合作情况信息

--- src/main/java/com/tsbg/ecosys/controller/CompanyController.java
+++ src/main/java/com/tsbg/ecosys/controller/CompanyController.java
@@ -1,9 +1,14 @@
 package com.tsbg.ecosys.controller;
 
 import com.tsbg.ecosys.config.ResultResponse;
+import com.tsbg.ecosys.model.EcInfo;
+import com.tsbg.ecosys.model.Ecooperation;
 import com.tsbg.ecosys.service.EcInfoService;
+import com.tsbg.ecosys.service.EcooperationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 /**
  * 公司相关
@@ -14,23 +19,60 @@ public class CompanyController {
 
     @Autowired
     private EcInfoService ecInfoService;
+    @Autowired
+    private EcooperationService ecooperationService;
 
     /**
      * 管理员隐藏公司
      */
     @RequestMapping(value = "/hideCompany", method = { RequestMethod.GET, RequestMethod.POST })
     @ResponseBody
-    public ResultResponse hideCom(@RequestParam int cid){
+    public ResultResponse hideCom(@RequestBody EcInfo ecInfo){
         //初始化构造器
         ResultResponse resultResponse = null;
-        //根据前端传过来的cid作为参数修改公司的状态
-        int num = ecInfoService.updateByCid(cid);
-        if (num>0){
-            resultResponse = new ResultResponse(0,"提示信息：隐藏公司成功！");
+        //获取cid
+        Integer cid = ecInfo.getCid();
+        if (cid!=null){
+            //根据前端传过来的cid作为参数修改公司的状态
+            int num = ecInfoService.updateByCid(cid);
+            if (num>0){
+                resultResponse = new ResultResponse(0,"提示信息：隐藏公司成功！");
+                return resultResponse;
+            }
+            //如果没有修改成功则返回失败
+            resultResponse = new ResultResponse(501,"提示信息：隐藏公司失败！");
             return resultResponse;
         }
-        //如果没有修改成功则返回失败
-        resultResponse = new ResultResponse(501,"提示信息：隐藏公司失败！");
+        //公司编号为空则返回异常信息
+        resultResponse = new ResultResponse(502,"提示信息：公司编号为空异常！");
+        return resultResponse;
+    }
+
+    /**
+     * 合作情况信息新增
+     */
+    @RequestMapping(value = "/addCooperation", method = { RequestMethod.GET, RequestMethod.POST })
+    @ResponseBody
+    public ResultResponse addCoo(@RequestBody Ecooperation ecooperation){
+        /*Ecooperation ecooperation = new Ecooperation("阿里巴巴",new Date(),"test"
+        ,"合作开始阶段",0,"昨天",1,"腾讯",0,"生态系统",
+                "强强合作","进展顺利","mis",new Date(),"张三");*/
+        //初始化传参构造器
+        ResultResponse resultResponse = null;
+        //需要从前台获取合作伙伴编号和其他合作情况信息
+        if (ecooperation!=null){
+            //设置当前时间为创建时间，暂时忽略创建人
+            ecooperation.setCreateTime(new Date());
+            //调用业务逻辑增加前端传来的信息至数据库
+            int num = ecooperationService.insertSelective(ecooperation);
+            if (num>0){
+                resultResponse = new ResultResponse(0,"提示信息：增加成功");
+                return resultResponse;
+            }
+            resultResponse = new ResultResponse(501,"提示信息：新增失败");
+            return resultResponse;
+        }
+        resultResponse = new ResultResponse(502,"提示信息：请输入信息");
         return resultResponse;
     }
 }
