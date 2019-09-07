97d34e9f581d5109f809348dcae7d7e5f9b8e0ab
zhoucongming <18598056737@163.com>
2019-09-07 08:59:56 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/tsbg/ecosys/common/NeedLogin.java
+++ src/main/java/com/tsbg/ecosys/common/NeedLogin.java
@@ -2,11 +2,7 @@ package com.tsbg.ecosys.common;
 
 import java.lang.annotation.*;
 
-/**
- * @author wzh
- * @version 1.0.0
- * @date 2019 19:16
- */
+
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
