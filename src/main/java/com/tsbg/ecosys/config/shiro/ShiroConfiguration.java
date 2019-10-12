643570ba246ee297fdb57b23ac9881c086894ee6
ElvisJonathan <chenq0711@sina.com>
2019-10-12 15:02:10 +0800

    遗漏

--- src/main/java/com/tsbg/ecosys/config/shiro/ShiroConfiguration.java
+++ src/main/java/com/tsbg/ecosys/config/shiro/ShiroConfiguration.java
@@ -1,7 +1,7 @@
 package com.tsbg.ecosys.config.shiro;
 
-/*
 import com.tsbg.ecosys.common.CustomSessionManager;
+import com.tsbg.ecosys.filter.CorsFilter;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.session.mgt.SessionManager;
@@ -19,36 +19,31 @@ import javax.servlet.Filter;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-*/
 /**
  *  shiro配置类
- *//*
-
+ */
 @Configuration
 public class ShiroConfiguration {
-	*/
-/**
+	/**
 	 * Shiro的Web过滤器Factory 命名:shiroFilter
-
+	 */
 	@Bean(name = "shiroFilter")
 	public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
 		ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
 		//Shiro的核心安全接口,这个属性是必须的
 		shiroFilterFactoryBean.setSecurityManager(securityManager);
 		Map<String, Filter> filterMap = new LinkedHashMap<>();
-		filterMap.put("authc", new AjaxPermissionsAuthorizationFilter());
+		filterMap.put("cors",new CorsFilter()); //跨域
+		filterMap.put("auth", new AjaxPermissionsAuthorizationFilter());
 		shiroFilterFactoryBean.setFilters(filterMap);
 		/*定义shiro过滤链  Map结构
 		 * Map中key(xml中是指value值)的第一个'/'代表的路径是相对于HttpServletRequest.getContextPath()的值来的
 		 * anon：它对应的过滤器里面是空的,什么都没做,这里.do和.jsp后面的*表示参数,比方说login.jsp?main这种
 		 * authc：该过滤器下的页面必须验证后才能访问,它是Shiro内置的一个拦截器org.apache.shiro.web.filter.authc.FormAuthenticationFilter
-		 *//*
-
+		 */
 		Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
-         */
-/* 过滤链定义，从上向下顺序执行，一般将 / ** 放在最为下边:这是一个坑呢，一不小心代码就不好使了;
-          authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问 *//*
-
+         /* 过滤链定义，从上向下顺序执行，一般将 / ** 放在最为下边:这是一个坑呢，一不小心代码就不好使了;
+          authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问 */
 		filterChainDefinitionMap.put("/", "anon");
 		filterChainDefinitionMap.put("/static/**", "anon");
 		filterChainDefinitionMap.put("/tsbg/register/ecosign", "anon");
@@ -59,16 +54,14 @@ public class ShiroConfiguration {
 		filterChainDefinitionMap.put("/login/logout", "anon");
 		filterChainDefinitionMap.put("/error", "anon");
 		filterChainDefinitionMap.put("/tsbg/power/powerdetail", "anon");
-		filterChainDefinitionMap.put("/**", "anon");
+		filterChainDefinitionMap.put("/**", "cors,anon");
 		shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
 		return shiroFilterFactoryBean;
 	}
 
-	*/
-/**
+	/**
 	 * 不指定名字的话，自动创建一个方法名第一个字母小写的bean
-	 *//*
-
+	 */
 	@Bean
 	public SecurityManager securityManager() {
 		DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
@@ -77,26 +70,22 @@ public class ShiroConfiguration {
 		return securityManager;
 	}
 
-	*/
-/**
+	/**
 	 * Shiro Realm 继承自AuthorizingRealm的自定义Realm,即指定Shiro验证用户登录的类为自定义的
-	 *//*
-
+	 */
 	@Bean
 	public UserRealm userRealm() {
 		UserRealm userRealm = new UserRealm();
 		return userRealm;
 	}
 
-	*/
-/**
+	/**
 	 * 凭证匹配器
 	 * （由于我们的密码校验交给Shiro的SimpleAuthenticationInfo进行处理了
 	 * 所以我们需要修改下doGetAuthenticationInfo中的代码;
 	 * ）
 	 * 可以扩展凭证匹配器，实现 输入密码错误次数后锁定等功能，下一次
-	 *//*
-
+	 */
 	@Bean(name = "credentialsMatcher")
 	public HashedCredentialsMatcher hashedCredentialsMatcher() {
 		HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
@@ -109,22 +98,18 @@ public class ShiroConfiguration {
 		return hashedCredentialsMatcher;
 	}
 
-	*/
-/**
+	/**
 	 * Shiro生命周期处理器
-	 *//*
-
+	 */
 	@Bean
 	public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
 		return new LifecycleBeanPostProcessor();
 	}
 
-	*/
-/**
+	/**
 	 * 开启Shiro的注解(如@RequiresRoles,@RequiresPermissions),需借助SpringAOP扫描使用Shiro注解的类,并在必要时进行安全逻辑验证
 	 * 配置以下两个bean(DefaultAdvisorAutoProxyCreator(可选)和AuthorizationAttributeSourceAdvisor)即可实现此功能
-	 *//*
-
+	 */
 	@Bean
 	@DependsOn({"lifecycleBeanPostProcessor"})
 	public DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator() {
@@ -143,13 +128,10 @@ public class ShiroConfiguration {
 	@Bean("sessionManager")
 	public SessionManager sessionManager(){
 		CustomSessionManager manager = new CustomSessionManager();
-     */
-/*使用了shiro自带缓存，
+     /*使用了shiro自带缓存，
      如果设置 redis为缓存需要重写CacheManager（其中需要重写Cache）
-     manager.setCacheManager(this.RedisCacheManager());*//*
-
+     manager.setCacheManager(this.RedisCacheManager());*/
 		manager.setSessionDAO(new EnterpriseCacheSessionDAO());
 		return manager;
 	}
 }
-*/
